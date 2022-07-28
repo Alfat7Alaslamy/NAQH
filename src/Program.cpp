1483376b3b4ceeda230b2190876e4d96057661a4
Alsraj <alsragbode@gmail.com>
2022-07-28 13:47:50 +0200

    re ReDev v0.1

--- src/Program.cpp
+++ src/Program.cpp
@@ -2,12 +2,18 @@
 
 int main()
 {
-    Window window("NAQH v0.1", 680, 840);
+    Window window("NAQH v0.1", 800, 600);
+    glClearColor(0.2f, 0.6f, 0.8f, 1.0f);
 
     while (!window.Closed())
     {
         window.Clear();
-        
+        glBegin(GL_QUADS);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(-0.5, 0.5);
+        glVertex2f(0.5, -0.5);
+        glVertex2f(0.5, 0.5);
+        glEnd();
         window.Update();
     }
     
